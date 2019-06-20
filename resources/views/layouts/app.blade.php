3894811433d570328e2b849db2fd3629ed21f4fc
Mohammad Ali Ahantab <m.x10aa@gmail.com>
2019-06-20 14:33:28 +0430

    app blade update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-@include('script')
+@include('layouts.script')
 </head>
 <body>
     <div id="app">
