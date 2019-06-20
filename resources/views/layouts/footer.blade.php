a36e3fe49c6cde080af7b267dabea946817a1002
Mohammad Ali Ahantab <m.x10aa@gmail.com>
2019-06-20 14:33:08 +0430

    footer added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="ftr">
+    <div class="row">
+        <div class="col-sm-4"> <i class="fas fa-phone-square-alt"></i> 02126400130</div>
+        <div class="col-sm-4">&copy; 2019 Copyright : Fiteon.ir</div>
+        <div class="col-sm-4"><i class="fas fa-envelope-square"></i> info@fiteon.ir</div>
+    </div>
+</div>
