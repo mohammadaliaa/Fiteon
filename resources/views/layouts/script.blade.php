3ed14234522efa3774c7eaf230a1f2d58db4454b
Mohammad Ali Ahantab <m.x10aa@gmail.com>
2019-06-25 15:49:36 +0430

    font iran sans added for persian

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -5,24 +5,36 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" />
 {{-- font awesome --}}
 <script src="https://kit.fontawesome.com/238c528f06.js"></script>
-    <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<!-- Scripts -->
+{{--
+<script src="{{ asset('js/app.js') }}" defer></script>
+--}}
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com" />
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
 
-{{-- <!-- include summernote css/js -->
-<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script> --}}
-
+{{--
+<!-- include summernote css/js -->
+<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet" />
+<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
+--}}
 
 <!-- Core build with no theme, formatting, non-essential modules -->
-<link href="//cdn.quilljs.com/1.3.6/quill.core.css" rel="stylesheet">
+<link href="//cdn.quilljs.com/1.3.6/quill.core.css" rel="stylesheet" />
 <script src="//cdn.quilljs.com/1.3.6/quill.core.js"></script>
 <!-- Main Quill library -->
 <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
 <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
 
 <!-- Theme included stylesheets -->
-<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-<link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
+<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
+<link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet" />
+
+@if (app()->getLocale()== 'fa')
+<style>
+    body {
+        font-family: IRANSans !important;
+        font-weight: 200;
+    }
+</style>
+@endif
