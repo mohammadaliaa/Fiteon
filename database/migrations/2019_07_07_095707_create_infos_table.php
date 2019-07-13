b9b1d20cff52a4b15c07ee60a10ae12b0d470868
Mohammad Ali Ahantab <m.x10aa@gmail.com>
2019-07-13 11:08:12 +0430

    start fiteon edit 2

--- database/migrations/2019_07_07_095707_create_infos_table.php
+++ database/migrations/2019_07_07_095707_create_infos_table.php
@@ -23,8 +23,13 @@ class CreateInfosTable extends Migration
             $table->string('lat');
             $table->string('lng');
             $table->string('address_1');
+            $table->string('address_1_fa');
             $table->string('address_2')->nullable();
+            $table->string('address_2_fa')->nullable();
             $table->string('address_3')->nullable();
+            $table->string('address_3_fa')->nullable();
+            $table->longText('aboutus');
+            $table->longText('aboutus_fa');
         });
     }
 
